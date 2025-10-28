0ea9f9fad80bff279a4b0c54346fa4dea6e2af89
Abdallah Abusamrah <abdallahabuamrah@gmail.com>
2025-10-28 08:27:42 +0100

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: {
     buildActivity: true,
-    buildActivityPosition: 'bottom-right',
+    buildActivityPosition: "bottom-right",
   },
 };
 
